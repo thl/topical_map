26a7e39e80704aa4be5fca4461a7ba78f40766c9
Jed Verity <jev3a@virginia.edu>
2010-12-16 18:05:01 -0500

    new test code for iframe

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,8 @@
 jQuery(document).ready(function(){
   jQuery("#sideMenuLink").css("background-position", "0% 0%");
   jQuery("#fxSideMenu").hide();
+  if(top != self){
+	var content = jQuery('#content').wrap("<div />").parent().html();
+	jQuery('#main-wrapper').html( content );
+  }
 });
