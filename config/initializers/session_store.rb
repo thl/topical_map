5e40eec54df687faa2996ba688ea4a6ea00c19dd
Andres Montano <amontano@virginia.edu>
2012-05-31 14:53:27 -0600

    updating session cookie domain, adding passiverecord and updating engines

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Kmaps::Application.config.session_store :cookie_store, :key => ThlCookie.session_key
+Kmaps::Application.config.session_store :cookie_store, :key => ThlCookie.session_key, :domain => '.thlib.org'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
